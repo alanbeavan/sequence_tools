e0943b167481ded393c6607febbf289ffe54cf5b
Alan Beavan <alan.beavan@bristol.ac.uk>
2019-08-06 11:13:29 +0100

    modified reciprocal_blast.py

--- reciprocal_blast.py
+++ reciprocal_blast.py
@@ -7,30 +7,31 @@ from my_module import *
 #takes a fasta as input and loops through the blast databases in a directory, checking if the best hit is for each gene in the gene family
 #corresponds to the representative of that species that is present in the fasta file
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print("Usage:  python3 fasta_file db_directory\n\ndb_directory can be full or relative path. We're easy")
     exit()
 else:
     seq_file = sys.argv[1]
     db_directory = sys.argv[2]
+    output_directory = sys.argv[3]
 
 seqs = get_file_data(seq_file)
+seq_name = seq_file.split("/")[-1]
 files = os.listdir(db_directory)
 dbs = sorted(set([i.split(".", 1)[0] for i in files]))
-
+fail = 0
 for db in dbs:
-    subprocess.run("blastp -query " + seq_file + " -db " + db_directory + "/"  + db + " -out out_temp -outfmt 6 -evalue 1e-09 -num_alignments 1", shell = True)
-    lines = get_file_data("out_temp")
+    subprocess.run("blastp -query " + seq_file + " -db " + db_directory + "/"  + db + " -out " + output_directory + "/" + seq_name + "_vs_" + db + " -outfmt 6 -evalue 1e-09 -num_alignments 1", shell = True)
+    lines = get_file_data(output_directory + "/" + seq_name + "_vs_" + db)
     for line in lines:
         hit = line.split("\t")[1]
         r = re.compile(hit)
         if len(list(s for s in seqs if hit in s)) >= 1:
-#            print(line)
             next
         else:
-#            print(line)
-            print(seq_file + " reciprocicity not fullfilled")
-            os.remove("out_temp")
-            exit()
-    os.remove("out_temp")
-print(seq_file + " reciprocicity fullfilled")
+            fail = 1
+
+if fail:
+    print(seq_file + " reciprocicity not fullfilled")
+else:
+    print(seq_file + " reciprocicity fullfilled")
