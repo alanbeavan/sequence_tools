cc22e1545c14a930727052edb7078a7df073685c
Alan Beavan <alan.beavan@bristol.ac.uk>
2019-08-07 11:09:29 +0100

    added fasta_to_phylip.py

---
+++ fasta_to_phylip.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.6
+
+import sys
+sys.path.append("/Users/ab17362/OneDrive - University of Bristol/Python_modules")
+from my_module import *
+
+fasta_to_phylip(sys.argv[1], sys.argv[2])
